39a765a01aae047644d34f154f0e3e7d3f6bfc93
leloufadel <leloufadel@gmail.com>
2024-03-28 16:25:37 +0000

    Use bind method

--- script.js
+++ script.js
@@ -96,22 +96,18 @@ clicked.classList.add('operations__tab--active');
 });
 //
 // refactor : 
-const handleHover = function(e, opacity) {
+const handleHover = function(e) {
   if(e.target.classList.contains('nav__link')){
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
     siblings.forEach(el => {
-      if (el !== link) el.style.opacity = opacity;
+      if (el !== link) el.style.opacity = this;
     });
-    logo.style.opacity = opacity;
+    logo.style.opacity = this;
   }
 }
 // Menu fade animation
 
-nav.addEventListener('mouseover', function (e){
-  handleHover(e, 0.5)
-});
-nav.addEventListener('mouseout', function (e){
-  handleHover(e, 1)
-});
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
